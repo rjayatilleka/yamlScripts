4c3271de074a05411b1bd914fcdf8f21c023c9a5
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-12-04 11:19:26 -0600

    read from stdin works

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 yaml = require('js-yaml')
-fs   = require('fs')
 argv = require('minimist')(process.argv.slice(2))
 
-console.log('working')
-
-var doc = yaml.safeLoad(fs.readFileSync(argv.input, 'utf8'));
-console.log(doc);
+var content = ''
+process.stdin.resume()
+process.stdin.on('data', function(buf) {
+  content += buf.toString()
+})
+process.stdin.on('end', function() {
+  //var doc = yaml.safeLoad(fs.readFileSync(argv.input, 'utf8'))
+  var doc = yaml.safeLoad(content)
+  console.log(doc)
+})
 
 
