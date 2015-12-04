423cc0caaf171e3a7c03c0bca79a53b36ce5dddc
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2015-12-04 11:11:10 -0600

    ignore

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 yaml = require('js-yaml')
 fs   = require('fs')
+argv = require('minimist')(process.argv.slice(2))
 
 console.log('working')
 
-var doc = yaml.safeLoad(fs.readFileSync('/Users/rjayatilleka/Dropbox/code/emcee/src/Patching/ls2015.yaml', 'utf8'));
+var doc = yaml.safeLoad(fs.readFileSync(argv.input, 'utf8'));
 console.log(doc);
+
+
